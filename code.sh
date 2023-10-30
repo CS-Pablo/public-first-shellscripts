2297f668320476c37d14e400a2ae65a6eace8649
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-30 16:34:54 +0100

    Update code.sh

---
+++ code.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Commentaire : Ce script fait quelque chose d'utile
+
+# Déclarations de variables
+variable1="valeur1"
+variable2="valeur2"
+
+# Fonction pour effectuer une tâche
+ma_fonction() {
+    # Instructions de la fonction
+    echo "La fonction a été appelée avec l'argument : $1"
+    # Ajoutez ici le code que vous souhaitez exécuter dans la fonction
+}
+
+# Instructions principales du script
+echo "Début du script"
+
+# Utilisation de la fonction
+ma_fonction "argument"
+
+# Plus d'instructions
+
+# Fin du script
+echo "Fin du script"
